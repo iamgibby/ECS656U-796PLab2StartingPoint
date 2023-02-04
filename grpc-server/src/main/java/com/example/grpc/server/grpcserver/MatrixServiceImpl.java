b000bd943a7d0a55eca4afc87e59e989c3048829
iamgibby <71736174+iamgibby@users.noreply.github.com>
2023-02-04 20:19:48 +0000

    Update MatrixServiceImpl.java

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -22,10 +22,10 @@ public class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase
   public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
     	{
         System.out.println("Request received from client:\n" + request);
-        int C00=request.getA00()*request.getB00()+request.getA01()*request.getB10();//good
+				int C00=request.getA00()*request.getB00()+request.getA01()*request.getB10();//good
+				int C10=request.getA10()*request.getB00()+request.getA11()*request.getB10();//good
 				int C01=request.getA00()*request.getB01()+request.getA01()*request.getB11();//good
-				int C10=request.getA10()*request.getB10()+request.getA11()*request.getB10();//good
-				int C11=request.getA10()*request.getB01()+request.getA11()*request.getB11();//good
+				int C11=request.getA10()*request.getB01()+request.getA11()*request.getB11();
         MatrixReply response = MatrixReply.newBuilder().setC00(C00).setC01(C01).setC10(C10).setC11(C11).build();
         reply.onNext(response);
         reply.onCompleted();
